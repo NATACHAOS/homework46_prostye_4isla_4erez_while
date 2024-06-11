bfb8e548eb77bd2eb7f5d83c633335a30c40082f
NATACHAOS <164019299+NATACHAOS@users.noreply.github.com>
2024-06-11 15:51:33 +0300

    Add files via upload

--- while_prostye_4isla.py
+++ while_prostye_4isla.py
@@ -1,15 +1,15 @@
 """Найти простые числа в диапазоне введённого числа"""
 
-a = int(input("Введите число: "))
+a = int(input("Введите число: ")) # вводим число
 flag = True
-num = [x for x in range(1, a + 1)]
+num = [x for x in range(1, a + 1)] # выведу простые числа от 1 до введённого числа
 d = 2
-spisok_s_prostymi_4islami = []
+spisok_s_prostymi_4islami = []  # простые числа попадают сюда
 while d * d <= a:
     if a % d == 0:
         flag = False
         break
     d += 1
-else:
-    spisok_s_prostymi_4islami.append(num)
+    if flag:   
+        spisok_s_prostymi_4islami.append(num)
 print(f'{spisok_s_prostymi_4islami} - Простые числа в диапазоне от 1 до {a}')
